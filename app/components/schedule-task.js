71fff6c36537ce23c890039ddd0f7cb66a5197f6
priyanka <priyankadutta67@gmail.com>
2018-11-14 12:39:46 +0530

    long press implemented

--- app/components/schedule-task.js
+++ app/components/schedule-task.js
@@ -1,9 +1,36 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-    actions: {
-        selectCategory(category) {
-            this.selectTask(category.text);
-        }
-    }
-});
+    
+      startTime: null,
+      endTime: null,
+      selectedBands: [],
+      selected: false,
+      selectedCount: Ember.computed.reads('selectedBands.length'),
+    
+      actions: {
+          
+        selectBand(event) {
+            
+            this.set('startTime', new Date().getTime())
+            // debugger
+            console.log(event.text)
+            event.enable 
+         const selectedBands = Ember.$(event.text.target).val();
+         this.set('selectedBands', selectedBands || []);
+       },   
+       unselectBand(item) {
+           
+            if((this.startTime + 500) < new Date().getTime()){
+                console.log("Long Press")
+                this.set('selected', true)
+
+            }
+       },
+       hello() {
+           
+       }
+       
+     }
+   });
+   
