069cb4b55432943eefbba7180a9d92d748363032
priyanka <priyankadutta67@gmail.com>
2018-11-15 10:54:07 +0530

    activity-plan and schedule-task components changed to activity-planner-activity-plan and activity-planner-schedulled

--- tests/integration/components/activity-planner-activity-plan-test.js
+++ tests/integration/components/activity-planner-activity-plan-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | activity-plan', function(hooks) {
+module('Integration | Component | activity-planner-activity-plan', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`{{activity-plan}}`);
+    await render(hbs`{{activity-planner-activity-plan}}`);
 
     assert.equal(this.element.textContent.trim(), '');
 
     // Template block usage:
     await render(hbs`
-      {{#activity-plan}}
+      {{#activity-planner-activity-plan}}
         template block text
-      {{/activity-plan}}
+      {{/activity-planner-activity-plan}}
     `);
 
     assert.equal(this.element.textContent.trim(), 'template block text');
