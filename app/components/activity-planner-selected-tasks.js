a5064f7b00302f2741db20d8dfa2ecc0df44ef96
Swarnim Das <1405266@kiit.ac.in>
2018-11-20 08:37:08 +0530

    selected-task component render actions according to different views

--- app/components/activity-planner-selected-tasks.js
+++ app/components/activity-planner-selected-tasks.js
@@ -1,8 +1,43 @@
 import Component from '@ember/component';
+import {
+    computed
+  } from '@ember/object';
 
 export default Component.extend({
     randomtasks: ['swarnim','atreya'],
-    actions:{
+    renderView: 'updates',
+    actionComponent: computed('tabIndex', function() {
+        let tab = this.get('tabIndex');
+        console.log(tab,"HEREEEE");
+        switch(tab) {
+            case 0: this.set('renderView','updates');
+                    break;
+            case 1: this.set('renderView','scheduled');
+                    break;
+            case 2: this.set('renderView','backlogs');
+                    break;
+            case 3: this.set('renderView', 'activityPlan');
+                    break;
+            default: return;
+        }
+    }),
+
+    showUpdatesAction: computed('renderView',function(){
+        return this.renderView === 'updates'
+    }),
+
+    showScheduledAction: computed('renderView',function(){
+        return this.renderView === 'scheduled'
+    }),
+
+    showBacklogsAction: computed('renderView',function(){
+        return this.renderView === 'backlogs'
+    }),
+    showActivityPlanAction: computed('renderView',function(){
+        return this.renderView === 'activityPlan'
+    }),
+
+    actions: {
         raisedButton() {
             console.log(this.get('todayTeamCopy'));
             this.get('todayTeamCopy').pushObjects(this.get('selectedTasks'));
