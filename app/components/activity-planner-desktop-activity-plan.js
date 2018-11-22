1e18738a7ddcff8d6312bb88f9b8f075378fda8d
Anirudha11 <anirudha.padhee@gmail.com>
2018-11-22 10:26:13 +0530

    multiple tasks can be added to the respective components simultaneously from the input-texbox

--- app/components/activity-planner-desktop-activity-plan.js
+++ app/components/activity-planner-desktop-activity-plan.js
@@ -16,74 +16,195 @@ export default Component.extend({
             this.activityPlanTasks.pushObject(x);
             console.log(this.activityPlanTasks);
         },
-            add() {
-              if (this.getProperties('input').input) {
-                  let s = this.getProperties('input').input;
-                  let taskName = s.split('@');
-                  console.log(taskName,"taskName");
+        add() {
+            if (this.getProperties('input').input) {
+                let s = this.getProperties('input').input;
+                // let taskName = s.split('@');
+                // console.log(taskName,"taskName");
                 //   let word = s.split(' ');
                 //   console.log(word,"array of space seperated strings");
                 //   word.map(function(e){
-                //       if(e.charAt(0)=== "@"){
-
+                    //       if(e.charAt(0)=== "@"){
+                        
+                        //       }
+                        //   })
+                        console.log(this,"this1")
+                        let multi = s.split('+');
+                        console.log(multi,"multi")
+                       if(multi.length>=0){
+                           let that = this;
+                           console.log(that,"that3")
+                            multi.map(function(e){
+                                let taskName = e.split('@');
+                                console.log(taskName,"taskName");
+                                if(taskName.length>1){
+                                    let x = taskName[1].substring(0);
+                                    // console.log(z,"asdadj")
+                                    console.log(x,"x")
+                                    if(x==="backlogs"){
+                                        let newTask = {
+                                            tasks:{
+                                                // text:this.getProperties('input').input,
+                                                text: taskName[0]
+                                            } 
+                                        }
+                                        console.log(this,"this");
+                                        that.backlogTasks.pushObject(newTask);
+                                    }
+                                    else if(x==="new"){
+                                        let y = {
+                                            tasks:{
+                                                text: taskName[0]
+                                            }
+                                        }
+                                        that.newTasks.pushObject(y);
+                                    }
+                                    else if(x==="pending"){
+                                        let newTask = {
+                                    
+                                tasks: {
+                                    text: taskName[0]
+                                }
+                            }
+                            that.pendingTasks.pushObject(newTask);
+                        }
+                        
+                        else {
+                            let newTask = {
+                                tasks:{
+                                  // text:this.getProperties('input').input
+                                  text: taskName[0],
+                                  owner: taskName[1]
+                                } 
+                              }
+                              console.log(newTask,"ASDF")
+                            that.activityPlanTasks.pushObject(newTask);
+                        }
+                      }  
+                          else
+                        {
+                          let newTask = {
+                              tasks:{
+                                // text:this.getProperties('input').input,
+                                text: taskName[0],
+                                owner: taskName[1]
+                              } 
+                            }
+                            console.log(newTask,"ASD")
+                          that.activityPlanTasks.pushObject(newTask);
+                        }
+                            })
+                       } 
+              }
+              this.set('input','');
+            }     
+    },
+add1(){
+    if (this.getProperties('input').input) {
+        let s = this.getProperties('input').input;
+        // let taskName = s.split('@');
+        // console.log(taskName,"taskName");
+        //   let word = s.split(' ');
+        //   console.log(word,"array of space seperated strings");
+        //   word.map(function(e){
+            //       if(e.charAt(0)=== "@"){
+                
                 //       }
                 //   })
-             
-              if(taskName.length>1){
-                  let x = taskName[1].substring(0);
-
-                  console.log(x,"x")
-                  if(x==="backlogs"){
-                    let newTask = {
-                        tasks:{
-                          // text:this.getProperties('input').input
-                          text: taskName[0]
-                        } 
-                      }
-                    this.backlogTasks.pushObject(newTask);
-                }
-                else if(x==="new"){
-                    let y = {
-                        tasks:{
-                            text: taskName[0]
+                console.log(this,"this1")
+                let multi = s.split('+');
+                console.log(multi,"multi")
+               if(multi.length>=0){
+                   let that = this;
+                   console.log(that,"that3")
+                    multi.map(function(e){
+                        console.log(e,"E");
+                        if(e!==""){
+                            let taskName = e.split('@');
+                            console.log(taskName,"taskName");
+                            if(taskName.length>1){
+                                let x = taskName[1].substring(0);
+                                let y = taskName[0].substring(0);
+                                // console.log(z,"asdadj")
+                                console.log(x,"x")
+                                if(x==="backlogs"){
+                                    let newTask = {
+                                        tasks:{
+                                            // text:this.getProperties('input').input,
+                                            text: taskName[0]
+                                        } 
+                                    }
+                                    console.log(this,"this");
+                                    that.backlogTasks.pushObject(newTask);
+                                }
+                                else if(x==="new"){
+                                    let y = {
+                                        tasks:{
+                                            text: taskName[0]
+                                        }
+                                    }
+                                    that.newTasks.pushObject(y);
+                                }
+                                else if(x==="pending"){
+                                    let newTask = {
+                                
+                            tasks: {
+                                text: taskName[0]
+                            }
                         }
+                        that.pendingTasks.pushObject(newTask);
                     }
-                    this.newTasks.pushObject(y);
-                }
-                else if(x==="pending"){
-                    let newTask = {
-                        tasks: {
-                            text: taskName[0]
+                    
+                    else if(x!==""){
+                        let newTask = {
+                            tasks:{
+                              // text:this.getProperties('input').input
+                              text: taskName[0],
+                              owner: taskName[1]
+                            } 
+                          }
+                          console.log(newTask,"ASDF")
+                        that.activityPlanTasks.pushObject(newTask);
+                    }
+                  }  
+                      else
+                    {
+                      let newTask = {
+                          tasks:{
+                            // text:this.getProperties('input').input,
+                            text: taskName[0],
+                            owner: taskName[1]
+                          } 
                         }
+                        console.log(newTask,"ASD")
+                      that.activityPlanTasks.pushObject(newTask);
                     }
-                    this.pendingTasks.pushObject(newTask);
+                        }
+ 
+                    })
+               } 
+      }
+      this.set('input','');
+    },
+    keyDown(event) {
+        let self = this;
+        let a = this.getProperties('input');
+        let c =a.input;
+        if (event.keyCode === 13) {
+            console.log(a,"keystroke");
+            let b = {
+                tasks:{
+                    text:c
                 }
-                else {
-                    let newTask = {
-                        tasks:{
-                          // text:this.getProperties('input').input
-                          text: taskName[0],
-                          owner: taskName[1]
-                        } 
-                      }
-                      console.log(newTask,"ASDF")
-                    this.activityPlanTasks.pushObject(newTask);
-                }
-              } 
-              else
-              {
-                let newTask = {
-                    tasks:{
-                      // text:this.getProperties('input').input
-                      text: taskName[0],
-                      owner: taskName[1]
-                    } 
-                  }
-                  console.log(newTask,"ASD")
-                this.activityPlanTasks.pushObject(newTask);
-              }
-            
-              }
-            }     
+            }
+            console.log(b,"b")
+            console.log(this,"thisasdasdfmkciopxpobnniubiux sywbijnsdoncnxdoisemwjkopspokakwipqowasoq[olas[")
+            // self.send('submitform');
+            // let data = event.dataTransfer.setData('some_Object', JSON.stringify(this.content));
+            // Ember.run.debounce(self,self.get('add1'),400);
+            this.add1();
+            this.set('input','')
+            return false;
+        }
     }
 });
--- app/components/draggable-item.js
+++ app/components/draggable-item.js
@@ -31,25 +31,7 @@ export default Component.extend({
   dragLeave(event) {
     event.preventDefault();
     set(this, 'dragClass', 'deactivated');
-  },
-
-  dragOver(event) {
-    event.preventDefault();
-    set(this, 'dragClass', 'activated');
-  },
-  drop(event) {
-    let data = event.dataTransfer.getData('some_Object');
-    console.log(data,"drop1")
-  
-    let rawData = JSON.parse(data);
-    console.log(rawData,"rawData in drop");
-    this.sendAction('dropped', rawData);
-    // this.actions.some_action(data)
-    // this.sendAction('dragged', rawData);
-    // console.log(JSON.stringify(data),data,"drop");
-    this.taskData.sendData(rawData);
-    // this.newTasks.removeObject(data);
-    // console.log(this.actions.some_action(data),data, "which we are getting from the events")
-    set(this, 'dragClass', 'deactivated');
   }
+
+ 
 });
