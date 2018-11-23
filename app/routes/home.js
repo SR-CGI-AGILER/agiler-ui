d61dedd80b663cae888ff01d54832285624fbd13
priyanka <priyankadutta67@gmail.com>
2018-11-23 14:43:43 +0530

    activity-planner-scheduled ->get and post API

--- app/routes/home.js
+++ app/routes/home.js
@@ -3,15 +3,22 @@ import Ember from 'ember';
 
 export default Route.extend({
     teamCopy: Ember.inject.service(),
-    productBacklogs: Ember.inject.service(), 
+    productBacklogs: Ember.inject.service(),
+    scheduled:Ember.inject.service() ,
     async model(){
         let model = {};
         await this.teamCopy.getTeamCopy("2018-11-21","default").then(function(data) {
             model.teamCopy = data.payload.data
         })
-        await this.productBacklogs.getProductBacklog().then(function (data) {
-           model.productBacklogs = data.payload.data
-        })
+        // await this.productBacklogs.getProductBacklog().then(function (data) {
+        //    model.productBacklogs = data.payload.data
+        // })
+        await this.scheduled.getScheduledOn().then(function (data) {
+            model.scheduled = data.payload.data
+         })
+         await this.scheduled.getScheduledFor().then(function (data) {
+            model.scheduledfor = data.payload.data
+         })
         return model;
     }
 });
