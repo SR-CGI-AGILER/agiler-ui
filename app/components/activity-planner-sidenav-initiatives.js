d526e76917cf91933772243f579a92a55b85b5cc
Ankit-Mund <ankit7mund@gmail.com>
2018-11-30 16:51:24 +0530

    Backlogs

--- app/components/activity-planner-sidenav-initiatives.js
+++ app/components/activity-planner-sidenav-initiatives.js
@@ -19,13 +19,13 @@ export default Component.extend({
     actions :{
         showUsers(init){
             this.get('session').set('initiative',init)
-            console.log(this.get('session').initiative,"HATT YAHA SE");
+            // console.log(this.get('session').initiative,"HATT YAHA SE");
             this.showUsers(init)
             // this.reRenderView();
             const currentRouteName = this.get("routing.currentRouteName");
             const currentRouteInstant = getOwner(this).lookup(`route:${currentRouteName}`);
             currentRouteInstant.refresh();
-            console.log("IN COMPONENT")
+            // console.log("IN COMPONENT")
 
         }
 
--- app/routes/home.js
+++ app/routes/home.js
@@ -9,29 +9,18 @@ export default Route.extend({
     session: Ember.inject.service(),
     userInitiative : Ember.inject.service(),
 
-    beforeModel(){
-        debugger
-    },
 
     async model(param){
         let that = this;
-        debugger
         console.log(param.ifPublished);
-    //    await this.userInitiative.getInitiatives("swarnim@gmail.com").then(function(data){
-    //         console.log(data.data.initiative[0],"PLEASE AA JAO NA");
-    //         that.get('session').set('initiative',data.data.initiative[0])
-            
-    //     })
+ 
         let model = {};
         let initiative = that.get('session').initiative;
-    //    await console.log(initiative,"ASDASDSAD");
         var d = new Date();
-        // d.setDate(d.getDate() - 1);
         var day = ("0" + d.getDate()).slice(-2);
         var month = ("0" + (d.getMonth()+ 1)).slice(-2);
         var today = d.getFullYear() + "-" + (month) + "-" + (day);
         let checkPublish = param.ifPublished;
-        // console.log(checkPublish,"IN HOME");
         if(checkPublish) {
             console.log(initiative,"CONSOLE HERE")
             await this.teamCopy.getTeamCopy(today,initiative.initiativeName).then(function(data) {
@@ -39,7 +28,8 @@ export default Route.extend({
                 model.teamCopy = data.payload.data
             })
             await this.productBacklogs.getProductBacklog().then(function (data) {
-               model.productBacklogs = data.payload.data
+               console.log(data);
+                model.productBacklogs = data.payload.data
             })
             
             // await this.scheduled.getScheduledOn().then(function (data) {
